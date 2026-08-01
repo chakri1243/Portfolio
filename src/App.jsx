5cd5dbb482041cdc20073206912c314e5512cffe
Chakrapani <chakrapanigutha@gmail.com>
2026-08-01 11:35:41 +0530

    Initial portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="bg-[#050816] min-h-screen text-white">
+      <h1 className="text-5xl font-bold text-center pt-20">
+        AI Nexus Portfolio
+      </h1>
+
+      <p className="text-center mt-6 text-gray-400">
+        Designed & Developed by Gutta Chakrapani
+      </p>
+    </div>
+  );
+}
+
+export default App;
